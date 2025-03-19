99826138b8e19a9f77d4a9303b30508f6a879c63
EvLast <EvLast@noreply.gitcode.com>
2025-03-19 23:00:37 +0800

    初始项目

---
+++ src/test/java/org/com/campushikingai/CampusHikingAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.campushikingai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusHikingAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
